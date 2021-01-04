ed8206d181a7618b0ed7bfdb515ba12b42096035
galina <t030626@gmail.com>
2021-01-04 21:47:23 +0200

    Video input as separate component

---
+++ client/src/components/VideoInput.js
@@ -0,0 +1,33 @@
+import React from 'react'
+
+export const VideoInput = props => {
+    const number = `video-${props.number}`
+    return (
+        <div className="input-group mb-3">
+            <input
+                type="text"
+                id={number}
+                name={number}
+                className="form-control"
+                placeholder="Add video url"
+                value={props.value}
+                onChange={props.changeHandler}
+            />
+            {
+                props.last
+                ? <button
+                    className={'btn btn-success ml-2'}
+                    onClick={props.addVideoHandler}
+                >
+                    +
+                </button>
+                : <button
+                        className={'btn btn-danger ml-2'}
+                    >
+                        -
+                    </button>
+            }
+
+        </div>
+    )
+}
